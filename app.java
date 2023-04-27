8a2fd1b439bdc41d5ed16c656a2b98019b47a5b3
VALNEY MAIA <93133793+valneymaia@users.noreply.github.com>
2023-04-27 13:10:29 -0300

    Update app.java

--- app.java
+++ app.java
@@ -1,59 +1,105 @@
+package Teste;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.*;
 import java.util.Scanner;
 
-public class App {
-public static void main(String[] args) throws Exception {
-    Scanner scanner = new Scanner(System.in);  
-    //System.out.print("Digite seu nome: ");
-    //String nome = scanner.nextLine();
-    //System.out.print("Digite seu CPF: ");
-    //String cpf = scanner.nextLine();
-    //System.out.print("Digite seu saldo da conta: ");
-    // double saldoa = scanner.nextDouble();
+import Dados.Cartao;
+import Dados.CartaoPrata;
+import Dados.CartaoVIP;
+import Dados.Produto;
+import Repositorio.Cliente;
+import Repositorio.Estoque;
 
-    Repositorio estoque = new Repositorio();
+public class Main{
+    static Scanner teclado = new Scanner(System.in);
 
-    Dados produto1 = new Dados("Redmi note 11");
-    Dados produto2 = new Dados("Redmi note 10 pro");
-    Dados produto3 = new Dados("Redmi note 11s");
-    Dados produto4 = new Dados("Poco M5S");
+    public static void main(String[] args) throws Exception {
+        List<Produto> produtos = new ArrayList<Produto>();
 
-    estoque.adicionarProduto(produto1);
-    estoque.adicionarProduto(produto2);
-    estoque.adicionarProduto(produto3);
-    estoque.adicionarProduto(produto4);
+        /*
+        System.out.print("Digite seu nome: ");
+        String nome = teclado.nextLine();
 
-    System.out.println(estoque.getListaprodutos());
-    System.out.println();
-    
-    int escolha = 0;
-    do {
-        System.out.println("==== MENU DE OPÇÕES ====");
-        System.out.println("1. Comprar");
-        System.out.println("2. Lista de produtos");
-        System.out.println("3. ");
-        System.out.println("4. Sair");
+        System.out.print("Digite seu salário: ");
+        float salario = teclado.nextFloat();
+        */
 
-        escolha = scanner.nextInt();
 
-        switch (escolha) {
-            case 1:
-                System.out.println("Digite o índice do produto que deseja comprar:");
-                int indice = scanner.nextInt();
-                estoque.comprar(indice);
-                break;
-            case 2:
-                System.out.println(estoque.getListaprodutos());
-                break;
-            case 3:
-                
-            break;
-            case 4:
-                System.out.println("Saindo...");
-                break;
-            default:
-                System.out.println("Opção inválida.");
-                break;
-        }
-    } while (escolha != 4);
-}
-}
+        Cliente c1 = new Cliente("Seu ze", "123.444.567.00", 1000);
+        c1.criarCartao();
+        c1.printarCartao();
+
+        Produto sabonete = new Produto("Sabonete", 2.90f, 123);
+        Produto arroz = new Produto("Arroz branco", 4.90f, 666);
+        Produto acucar = new Produto("Açucar", 3.90f, 987);
+
+        produtos.add(sabonete);
+        produtos.add(arroz);
+        produtos.add(acucar);
+
+        Estoque estoque = new Estoque(produtos);
+        estoque.printar();
+
+        int escolha = 0;
+        do {
+            System.out.println("==== MENU DE OPÇÕES ====");
+            System.out.println("1. Comprar");
+            System.out.println("2. consultar limite");
+            System.out.println("3. Ser vip");
+            System.out.println("4. Cancelar vip");
+            System.out.println("5. Sair");
+
+            escolha = teclado.nextInt();
+
+            switch (escolha) {
+                case 1:
+                    System.out.println("Digite a opção desejada:\n(1) Comprar por nome\n(2) Comprar por código de barras\n");
+                    int escolha2 = teclado.nextInt();
+                    teclado.nextLine();
+                    if (escolha2 == 1) {
+                        System.out.print("Nome: ");
+                        String nome = teclado.nextLine();
+
+                        System.out.print("Quantidade: ");
+                        int quantidade = teclado.nextInt();
+                        c1.comprar(produtos, nome, quantidade);
+
+                    } else if (escolha2 == 2) {
+                        System.out.print("Codigo: ");
+                        int codigo = teclado.nextInt();
+
+                        System.out.print("Quantidade: ");
+                        int quantidade = teclado.nextInt();
+                        c1.comprar(produtos, codigo, quantidade);
+                    } else {
+                        System.out.println("opçao inválida.");
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("o limite do seu cartao e: ");
+                    c1.printarCartao();
+                    break;
+                case 3:
+                    System.out.println("desejar ser vip, pagando 50 reais e ganhando descontos? (1) sim, (2) nao?");
+                    int escolha3 = teclado.nextInt();
+                    teclado.nextLine();
+                    if (escolha3 == 1) {
+                        c1.tornarVIP();
+                    } else {
+                        System.out.println("opçao inválida.");
+                    }
+                    break;
+                case 4:
+                    c1.cancelarVIP();
+                    break;
+                case 5:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+                    break;
+            }
+        } while (escolha != 5);
+    }}
