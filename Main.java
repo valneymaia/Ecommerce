72c7be1ee0d2ea799cc527f0d6a938253a1f3b3b
VALNEY MAIA <93133793+valneymaia@users.noreply.github.com>
2023-05-11 20:32:55 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,46 +1,27 @@
 package Teste;
-import java.util.ArrayList;
-import java.util.*;
 import java.util.Scanner;
-import java.io.*;
-import Dados.Produto;
 import Dados.Cliente;
 import Dados.Suporte;
 import Repositorio.Repositorio_Produto;
 
+
 public class Main{
+
     static Scanner teclado = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
-        List<Produto> produtosNovos = new ArrayList<Produto>();
-        Scanner scannerProduto = new Scanner(new File("C:\\Users\\usuario\\IdeaProjects\\Ecomerce\\src\\Dados\\produtos.csv"));
-        scannerProduto.useDelimiter(";");
-        while(scannerProduto.hasNext()) {
-            System.out.println(scannerProduto.next());
-        }
-        /*
-        Produto sabonete = new Produto("Sabonete", 2.90f, 123);
-        Produto arroz = new Produto("Arroz Branco", 4.90f, 666);
-        Produto acucar = new Produto("Açucar", 3.90f, 987);
-        */
 
         Repositorio_Produto estoque = new Repositorio_Produto();
+
         Cliente c1 = new Cliente("Seu ze", "123.444.567.00", 1000);
+
         Suporte suporte1 = new Suporte();
         c1.criarCartao();
 
         int escolha = 0;
         int escolha3 = 1, quantidade;
         do {
-            System.out.println("========= ECOMMERCE RV ==========");
-            System.out.println("======= MENU DE OPÇÕES ========");
-            System.out.println("1. Comprar produtos");
-            System.out.println("2. Consultar Perfil");
-            System.out.println("3. Assinar  vip");
-            System.out.println("4. Cancelar vip");
-            System.out.println("5. Modificar estoque");
-            System.out.println("6. Sair");
-            System.out.println("===============================");
+            menu();
 
             escolha = teclado.nextInt();
 
@@ -53,8 +34,7 @@ public class Main{
 
                     if (escolha2 == 1) {
                         for(;;) {
-                            System.out.print("digite '0' para finalizar comprar:");
-                            System.out.print("Nome: ");
+                            System.out.print("Nome: [0] para finalizar ");
                             String nome = teclado.nextLine();
 
                             if(nome.strip().equals("0"))
@@ -70,8 +50,7 @@ public class Main{
                     }
                     else if (escolha2 == 2) {
                         for(;;) {
-                            System.out.println("digite '0' para finalizar comprar:");
-                            System.out.print("Codigo: ");
+                            System.out.print("Codigo: [0] para finalizar ");
                             int codigo = teclado.nextInt();
 
                             if(codigo == 0)
@@ -89,12 +68,14 @@ public class Main{
                     }
                     break;
                 case 2:
-                    c1.Perfil();
+                    System.out.println(c1);
                     break;
+
                 case 3:
                     System.out.println("desejar ser vip, pagando 40 reais e ganhar 20% descontos? (1) sim, (2) nao?");
                     escolha3 = teclado.nextInt();
                     teclado.nextLine();
+
                     if (escolha3 == 1) {
                         c1.tornarVIP();
                     }
@@ -102,12 +83,13 @@ public class Main{
                         System.out.println("opçao inválida.");
                     }
                     break;
+
                 case 4:
                     c1.cancelarVIP();
                     break;
 
                 case 5:
-                    suporte1.alterarEstoque(estoque, produtosNovos);
+                    suporte1.alterarEstoque(estoque);
                     break;
 
                 case 6:
@@ -120,7 +102,23 @@ public class Main{
             }
 
         } while (escolha != 6);
-    }}
+    }
+
+
+    public static void menu() {
+        System.out.println("""
+                \n========= ECOMMERCE RV ==========
+                ======== MENU DE OPÇÕES =========
+                1. Comprar produto
+                2. Consultar Perfil
+                3. Assinar  vip
+                4. Cancelar vip
+                5. Modificar estoque(Suporte)
+                6. Sair
+                =================================\s
+                """);
+    }
+}
 
 
 
